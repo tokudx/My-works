0514dd49f2c59769a25794ac62136fe2fdb43553
tokudx <tokudx5892@gmail.com>
2022-03-14 12:12:34 +0900

    Java_Practice ver. 0.0.2

--- Main.java
+++ Main.java
@@ -1,7 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-       System.out.println(10 / 4.0);
-       System.out.println(10 / 4);
-       System.out.println(10 % 4);
+       System.out.println(10000.1 + 5000.1);
     }
 }
