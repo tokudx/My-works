fbe5ad850674232d3dbbcd0374ea980715e34447
tokudx <tokudx5892@gmail.com>
2022-03-19 18:37:39 +0900

    a+b

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         int a = 10;
-        System.out.println(a);
+        int b = 20;
+        System.out.println(a + b);
     }
 }
