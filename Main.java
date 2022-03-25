c7eea403ff7e7d5362644005a06cae62656f8954
tokudx <tokudx5892@gmail.com>
2022-03-25 09:01:47 +0900

    p57

--- Main.java
+++ Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         String word1 = "こんにちは。";
-        String word2 = "わたしはJavaです。";
+        String word2 = "私はJavaです。";
         String word3 = word1 + word2;
-        System.out.println(word3);
+        System.out.println(word3.length());
     }
 }   
